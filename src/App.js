7c7ee6393c41312cc2608daf648bfefe10c4d78e
Vanshika Bhatnagar <vanshikabhatnagar11@gmail.com>
2020-07-05 16:39:05 +0530

    Loader added

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { useEffect, useState } from 'react'
-import { Card, CardBody, CardHeader, Col, Container, Row, Table } from 'reactstrap'
+import { Card, CardBody, CardHeader, Col, Container, Row, Table, Spinner } from 'reactstrap'
 import Header from './Header'
 import Axios from 'axios'
 import { BaseURL } from './Requests'
@@ -8,12 +8,17 @@ import { BaseURL } from './Requests'
 const App = () => {
   // console.log(process.env)
   const [AllEmployees, setAllEmployees] = useState([])
-
+  const [LoadingEmployees, setLoadingEmployees] = useState(false)
 
   useEffect(() => {
     // Fetch Employees
+    setLoadingEmployees(true)
     Axios.get(`${BaseURL}employee/get`).then(e => {
       setAllEmployees(e.data)
+      setLoadingEmployees(false)
+    }).catch(e => {
+      setLoadingEmployees(false)
+      console.log(e)
     })
   }, [])
 
@@ -27,27 +32,31 @@ const App = () => {
               <CardHeader className='bg-secondary text-white d-flex justify-content-center'>
                 Employees
               </CardHeader>
-              <CardBody>
-                <Table striped hover>
-                  <thead className='text-secondary'>
-                    <tr>
-                      <th>EmpID</th>
-                      <th>Name</th>
-                      <th>Phone Number</th>
-                    </tr>
-                  </thead>
-                  <tbody>
-                    {AllEmployees.map((emp, key) => {
-                      return (
-                        <tr key={key}>
-                          <td>{emp.empID}</td>
-                          <td>{emp.name}</td>
-                          <td>{emp.phone}</td>
-                        </tr>
-                      )
-                    })}
-                  </tbody>
-                </Table>
+              <CardBody>{
+                LoadingEmployees ?
+                  <Spinner color='primary' />
+                  :
+                  <Table striped hover>
+                    <thead className='text-secondary'>
+                      <tr>
+                        <th>EmpID</th>
+                        <th>Name</th>
+                        <th>Phone Number</th>
+                      </tr>
+                    </thead>
+                    <tbody>
+                      {AllEmployees.map((emp, key) => {
+                        return (
+                          <tr key={key}>
+                            <td>{emp.empID}</td>
+                            <td>{emp.name}</td>
+                            <td>{emp.phone}</td>
+                          </tr>
+                        )
+                      })}
+                    </tbody>
+                  </Table>
+              }
               </CardBody>
             </Card>
 
